7471bd3ab4e0fa27e60e9d393af962f6c0352023
Aryan Dev Chourasia <aryandev1906@gmail.com>
2022-10-12 15:35:47 +0530

    index file added

--- quiz_project/quiz_project/urls.py
+++ quiz_project/quiz_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views   ## './dot' here means the file we are importing is in the same folder.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home)
 ]
