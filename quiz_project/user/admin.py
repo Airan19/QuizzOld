50fde27b374004ebd6dcf2ff0a1f8b40bc154140
Aryan Dev Chourasia <aryandev1906@gmail.com>
2022-10-19 00:26:28 +0530

    Urls, views, database updated

--- quiz_project/user/admin.py
+++ quiz_project/user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import  UserDb, Options, SetofQuestions, Tags
 # Register your models here.
+
+admin.site.register(UserDb)
+admin.site.register(Options)
+admin.site.register(SetofQuestions)
+admin.site.register(Tags)
