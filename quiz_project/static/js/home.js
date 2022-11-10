18e062b14ad300685b1e6b1992086f972374a19a
PriyanshiDhuriya <95227063+PriyanshiDhuriya@users.noreply.github.com>
2022-11-10 23:07:36 +0530

    new changes

--- quiz_project/static/js/home.js
+++ quiz_project/static/js/home.js
@@ -1,7 +1,7 @@
 <script
-  src="https://code.jquery.com/jquery-3.6.1.js"
+  src=" https://code.jquery.com/jquery-3.6.1.js"
   integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
-  crossorigin="anonymous"
+  crossorigin="anonymous "
 ></script>;
 
 const splash = document.querySelector(".splash");
