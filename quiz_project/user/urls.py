28f1e7e9da855c8f8425d2a13c05238849c251bb
Aryan Dev Chourasia <aryandev1906@gmail.com>
2023-01-27 13:21:46 +0530

    More changes

--- quiz_project/user/urls.py
+++ quiz_project/user/urls.py
@@ -8,14 +8,14 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 
-from .views import UserView, signup, landing_page
+# from .views import UserView, signup, landing_page
 
 app_name = 'user'
 
 urlpatterns = [
-    path('', landing_page, name='landing_page'),
+    # path('', landing_page, name='landing_page'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login'), name='logout'),
-    path('profile/',  login_required(UserView.as_view()), name='profile'),
-    path('signup/', signup, name='signup')
+    # path('profile/',  login_required(UserView.as_view()), name='profile'),
+    # path('signup/', signup, name='signup')
 ]
--- quiz_project/user/views.py
+++ quiz_project/user/views.py
@@ -1,48 +1,48 @@
-# from django.shortcuts import render
-# # from django.http import HttpResponse
+# # from django.shortcuts import render
+# # # from django.http import HttpResponse
+# #
+# # def about_view(request,*args,**kwargs):
+# #   return render(request,"about.html",{})
+# #
+# # #def index(request):
+# # #    return HttpResponse("Hello world!")
+# # # Create your views here.
 #
-# def about_view(request,*args,**kwargs):
-#   return render(request,"about.html",{})
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import login, authenticate
+# from django.views.generic.detail import DetailView
 #
-# #def index(request):
-# #    return HttpResponse("Hello world!")
-# # Create your views here.
-
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.views.generic.detail import DetailView
-
-from .forms import SignUpForm
-
-
-class UserView(DetailView):
-  template_name = 'users/profile.html'
-
-  def get_object(self):
-    return self.request.user
-
-
-def landing_page(request):
-  return 'working...'
-  # return render(request, '../templates/home.html')
-
-
-def signup(request):
-    if request.method == 'POST':
-      form = SignUpForm(request.POST)
-      if form.is_valid():
-        print('USERS valid k andrr takk')
-        user = form.save()
-        raw_password = form.cleaned_data.get('password1')
-        user = authenticate(request, email=user.email, password=raw_password)
-        if user is not None:
-          login(request, user)
-        else:
-          print("user is not authenticated")
-        return redirect('users:profile')
-    else:
-      form = SignUpForm()
-    return render(request, 'users/signup.html', {'form': form})
-
-def profile(request):
-  return 'redirected to profile'
+# from .forms import SignUpForm
+#
+#
+# class UserView(DetailView):
+#   template_name = 'users/profile.html'
+#
+#   def get_object(self):
+#     return self.request.user
+#
+#
+# def landing_page(request):
+#   return 'working...'
+#   # return render(request, '../templates/home.html')
+#
+#
+# def signup(request):
+#     if request.method == 'POST':
+#       form = SignUpForm(request.POST)
+#       if form.is_valid():
+#         print('USERS valid k andrr takk')
+#         user = form.save()
+#         raw_password = form.cleaned_data.get('password1')
+#         user = authenticate(request, email=user.email, password=raw_password)
+#         if user is not None:
+#           login(request, user)
+#         else:
+#           print("user is not authenticated")
+#         return redirect('users:profile')
+#     else:
+#       form = SignUpForm()
+#     return render(request, 'users/signup.html', {'form': form})
+#
+# def profile(request):
+#   return 'redirected to profile'
